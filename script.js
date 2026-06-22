813f9df3156dc0e0ac286368d331c651722283d7
Matias Ariel Castro <matias.acastro1@gmail.com>
2026-06-22 19:02:11 -0300

    menu hamburguesa funciona

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log('hola mundo')
+const hamburguesa = document.querySelector('.menu-nav__hamburguesa');
+const lista = document.querySelector('.menu-nav__lista');
+
+hamburguesa.addEventListener('click', () => {
+  lista.classList.toggle('menu-nav__lista--abierto');
+});
