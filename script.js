e5beb30c43ef93f5d63f49e0008ca6c2e9018ae9
Matias Ariel Castro <matias.acastro1@gmail.com>
2026-06-22 19:28:39 -0300

    modifica icono menu hamburguesa y cierre al navegar a seccion

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 const hamburguesa = document.querySelector('.menu-nav__hamburguesa');
 const lista = document.querySelector('.menu-nav__lista');
+const enlaces = document.querySelectorAll('.menu-nav__enlace, .menu-nav__btn-unirse');
 
-hamburguesa.addEventListener('click', () => {
+function toggleMenu() {
   lista.classList.toggle('menu-nav__lista--abierto');
+  hamburguesa.classList.toggle('menu-nav__hamburguesa--abierto');
+}
+
+hamburguesa.addEventListener('click', toggleMenu);
+
+enlaces.forEach(enlace => {
+  enlace.addEventListener('click', () => {
+    if (lista.classList.contains('menu-nav__lista--abierto')) {
+      toggleMenu();
+    }
+  });
 });
